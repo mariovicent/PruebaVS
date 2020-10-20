989111e7715d585ed59dd4692614d9d93f080f20
mariovicent <72019829+mariovicent@users.noreply.github.com>
2020-10-20 10:48:01 +0200

    inicio

--- prueba/prueba/Program.cs
+++ prueba/prueba/Program.cs
@@ -6,7 +6,7 @@ namespace prueba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
